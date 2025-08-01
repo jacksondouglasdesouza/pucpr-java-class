d792f11584039f97b58f6f3189b6943028996aa1
jacksondouglasdesouza <jacksondouglasdesouza@hotmail.com>
2025-08-01 05:13:01 -0300

    Classes of the Java Supplementary Course

---
+++ JavaCompletoProgramacaoOrientadaObjetos/class001.java
@@ -0,0 +1,20 @@
+package JavaCompletoProgramacaoOrientadaObjetos;
+import java.util.Scanner;
+
+public class class001 {
+
+	public static void main(String[] args) {
+		// ENTRADA DE DADOS EM JAVA SCANNER
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o seu primeiro nome: ");
+		String entradaDados =  scan.next();
+		
+		scan.close();
+				
+		System.out.printf("Ótimo, seja bem vindo %s ", entradaDados);
+		
+	}
+
+}
---
+++ JavaCompletoProgramacaoOrientadaObjetos/class001_1.java
@@ -0,0 +1,25 @@
+package JavaCompletoProgramacaoOrientadaObjetos;
+import java.util.Scanner;
+
+public class class001_1 {
+
+	public static void main(String[] args) {
+		// ENTRADA DE DADOS EM JAVA SCANNER
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int numero01, numero02;
+		
+		System.out.println("Digite um número inteiro: ");
+		numero01 =  scan.nextInt();
+		System.out.println("Digite outro número inteiro: ");
+		numero02 = scan.nextInt();		
+		int somaNumeros = numero01 + numero02;
+		
+		scan.close();
+				
+		System.out.printf("A soma dos números digitado é: %d", somaNumeros);
+		
+	}
+
+}
