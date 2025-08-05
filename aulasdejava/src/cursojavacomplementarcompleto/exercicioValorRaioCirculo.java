f8f25cfb6ed1bf5c7c0e966659da7b86c35a1523
jacksondouglasdesouza <jacksondouglasdesouza@hotmail.com>
2025-08-05 05:55:33 -0300

    Exercicios e Aulas, curso complementar de Java

---
+++ aulasdejava/src/cursojavacomplementarcompleto/exercicioValorRaioCirculo.java
@@ -0,0 +1,27 @@
+package cursojavacomplementarcompleto;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicioValorRaioCirculo {
+
+    public static void main(String[] args) {
+        // Faça um programa para ler o valor do raio de um círculo,
+        // e depois mostrar o valor da área deste círculo com quatro
+        //casas decimais.
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+
+
+        double PI = 3.14159;
+
+        System.out.printf("Digite o valor do raio para calcular: ");
+        double raio = scan.nextDouble();
+
+        raio = PI * Math.pow(raio, 2);
+
+        System.out.printf("A área do raio de de: %.4f", raio);
+
+    }
+}
